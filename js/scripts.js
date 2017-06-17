d232ef59bce9a2c8a3301d8d9c80cf68cf874e47
jcbcw <diannasinclar@aol.com>
2017-06-17 17:29:00 -0400

    Add overlay functionality to the call of action

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(function () {
-  $(".mobile-menu-icon").on("click", function (){
+  $(".mobile-menu-icon").on("click", function (event){
     $(".primary-nav").toggleClass('active');
     $(this).toggleClass("open");
+  });
+  $(".go-premium p a").on('click', function(event){
+    if (! $(".modal").hasClass('active')) {
+      $(".modal").addClass("active").fadeIn();
+    }
+  })
+  $('.close-modal a').on("click", function (event){
+    $(".modal").removeClass("active").fadeOut();
   });  
 });
